2e580b1c2deabfff797cc49bbc6bad68c0c3251b
heschwerdt <h.schwerdtner@tu-berlin.de>
2023-09-11 19:42:37 +0200

    added webextensionpolyfill

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,1 +1,15 @@
-console.log("Background script running...");
+import browser from "webextension-polyfill";
+
+if (browser.webRequest) {
+  const urlFilter = { urls: ["http://*/*", "https://*/*"] };
+
+  browser.webRequest.onBeforeSendHeaders.addListener(
+    (details) => {
+      console.log(details);
+    },
+    urlFilter,
+    ["requestHeaders", "extraHeaders", "blocking"]
+  );
+} else {
+  console.error("webRequest API is not available");
+}
