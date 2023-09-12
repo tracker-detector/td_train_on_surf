92d89a1e2907ace1da690b816a454da8e8869c1a
heschwerdt <h.schwerdtner@tu-berlin.de>
2023-09-12 23:55:41 +0200

    added line charts

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,14 +1,15 @@
 import { Nav } from "./components/Nav";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import useStore from "./store/store";
+import { LineChart } from '@mui/x-charts/LineChart';
+import Paper from '@mui/material/Paper';
+
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
 });
 function App() {
-  const currentPage = useStore((state) => state.currentPage)
 
   return (
     <ThemeProvider theme={darkTheme}>
@@ -18,7 +19,18 @@ function App() {
           Simple React Typescript Tailwind Sample
          
         </h1>
-         {currentPage}
+         <Paper elevation={2}>
+        <LineChart
+      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+      series={[
+        {
+          data: [2, 5.5, 2, 8.5, 1.5, 5],
+        },
+      ]}
+
+      height={300}
+          />
+          </Paper>
         <Nav />
       </main>
     </ThemeProvider>
